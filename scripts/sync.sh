adc67ef4a87a46bd87e86560f45d420289cc197a
vchavkov <vchavkov@assistance.bg>
2019-04-30 16:02:21 +0300

    + sync deploy script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,4 +2,5 @@
 
 NODE_ENV='production' gatsby build
 
-rsync -av --delete --stats ./public/ --exclude={"/dev/*","/var/log/*"} root@chavkov.com:/var/vhost/chavkov.com/
+# rsync -av --delete --stats ./public/ --exclude={"/dev/*","/var/log/*"} root@chavkov.com:/var/vhost/chavkov.com/
+rsync -av --delete --stats ./public/ --exclude={"/dev/*","/var/log/*"} root@assistance.bg:/var/vhost/assistance.bg/
